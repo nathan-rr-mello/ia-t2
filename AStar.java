98821a18528395a94ca21630d5570751bf61c7b3
Eduardo Menegaz Pretto <eduardo.pretto@edu.pucrs.br>
2023-11-21 10:16:09 -0300

    adicionando comentarios no A*

--- AStar.java
+++ AStar.java
@@ -1,51 +1,47 @@
 import java.util.*;
 public class AStar {
+
     public static void run (int[][] initialMatrix, int[][] finalMatrix, int x, int y) {
-    PriorityQueue<Board> open = new PriorityQueue<Board>(Comparator.comparingInt(board -> board.estimatedTotalCost));
-    Set<String> closed = new HashSet<String>();
-    Board initialBoard = new Board(initialMatrix, x, y);
-    Board finalState = new Board(finalMatrix);
-    boolean found = false;
-    //  0, calculateMisplacedTiles(initialMatrix),
+    PriorityQueue<Board> open = new PriorityQueue<Board>(Comparator.comparingInt(board -> board.estimatedTotalCost)); //PriorityQueue de nodos abertos
+    Set<String> closed = new HashSet<String>(); //Conjunto de nodos fechados
+    //esse conjunto é de strings para resolver um problema em que a comparação entre matrizes não funcionava corretamente
 
-    open.add(initialBoard);
-    while (!open.isEmpty()) {
+    Board initialBoard = new Board(initialMatrix, x, y); //inicializa estado inicial 
+    Board finalState = new Board(finalMatrix);           //e final
+
+    open.add(initialBoard); //adiciona o estado inicial na fila de prioridade
+    while (!open.isEmpty()) { //enquanto a fila de prioridade não estiver vazia
         
-        Board currentBoard = open.poll();
+        Board currentBoard = open.poll(); //retira o primeiro da fila
         
 
-        if (currentBoard.equals(finalState)) {
-            found = true;
-            System.out.println("\nnodes created: " + (closed.size() + open.size()));
-            System.out.println("\nPath generated: ");
+        if (currentBoard.equals(finalState)) { //testa se é a solução correta, se for: 
+            System.out.println("\nnodes created: " + (closed.size() + open.size())); //imprime o número de nodos criados
+            System.out.println("\nPath generated: "); //imprime o caminho gerado
             currentBoard.findPath(currentBoard);
-            return;
-        }
-        closed.add(Arrays.deepToString(currentBoard.state));
+            return; //finaliza a execução
+        } // e se não: 
 
-        List<Board> nextStates = Board.getNextStates(currentBoard);
-        for (Board nextBoard : nextStates) {
-            String nextBoardString = Arrays.deepToString(nextBoard.state);
-            if (!closed.contains(nextBoardString)) {
-                nextBoard.estimatedHeuristic = calculateMisplacedTiles(nextBoard.state);
+        closed.add(Arrays.deepToString(currentBoard.state)); //adiciona o estado atual no conjunto de nodos fechados
+
+        List<Board> nextStates = Board.getNextStates(currentBoard); //gera os filhos desse nodo
+
+        for (Board nextBoard : nextStates) { //percorre os filhos que podem ser obtidos
+
+            String nextBoardString = Arrays.deepToString(nextBoard.state); //método que transforma a matriz em uma string (para resolver o problema citado na linha 7)
+
+            if (!closed.contains(nextBoardString)) { //se o conjunto de nodos fechados não contém o filho gerado:
+                nextBoard.estimatedHeuristic = calculateMisplacedTiles(nextBoard.state); //calcula a heurística do filho
                 nextBoard.costFromStart = currentBoard.costFromStart + 1;
                 nextBoard.estimatedTotalCost = nextBoard.costFromStart + nextBoard.estimatedHeuristic;
-                // System.out.println("estimativas: " + nextBoard.estimatedHeuristic + " " + nextBoard.costFromStart + " " + nextBoard.estimatedTotalCost);
-
-                open.add(nextBoard);
-                // System.out.println(nextBoard);
+                open.add(nextBoard); //adiciona o filho na fila de prioridade
             }
         }
-
-        // System.out.println("----------------------");
     }
-    if (!found){
-        System.out.println("Não foi possível encontrar uma solução");
+        System.out.println("Não foi possível encontrar uma solução"); //se terminou a execução e não encontrou nenhum filho, não foi possível encontrar uma solução
     }
 
-    }
-
-    public static int calculateMisplacedTiles(int receivedMatrix[][]) {
+    public static int calculateMisplacedTiles(int receivedMatrix[][]) { //funcao heuristica que calcula quantas peças estão fora do lugar
         int finalMatrix[][] = {{1,2,3},{4,5,6},{7,8,0}};
         int misplacedTiles = 0;
         for(int i = 0; i < 3; i++){
